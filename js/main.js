7e2a0e0949a02cd5349cd85a8a9f2515f720cad2
Luis Mendoza <luis@mendoza.io>
2014-10-14 16:12:44 -0500

    Added initial home structure.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+$(document).ready( function(){
 
+	width = $(window).width();
+	height = $(window).height();
+
+	$('#page-container').css("margin-top", height + "px");
+
+} );
+
+
+$(window).resize( function(){
+
+	width = $(window).width();
+	height = $(window).height();
+
+	$('#page-container').css("margin-top", height + "px");
+	
+} );
