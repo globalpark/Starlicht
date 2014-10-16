c84329366cefdd74d12c1c329b1f96336792f0b2
Luis Mendoza <luis@mendoza.io>
2014-10-16 17:42:07 -0500

    Added Intro logo.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ $(document).ready( function(){
 	$('#page-container').css("margin-top", height + "px");
 	$('#foto-intro').height( height - $('#main-nav').height() - 42 );
 
+	$('#logo-intro').css("margin-top", (height - $('#logo-intro').height())/2 + "px" );
+
 	if( height < 740){
 		$('#container-quienes').css("margin-top", ($('#foto-intro').height()) * .4 + "px")
 	}else{
@@ -22,6 +24,7 @@ $(document).ready( function(){
 		$(this).children().children('.texto-marca').addClass('hidden');
 	});
 
+	$('#entrar').localScroll({ duration: 'slow', offset: 0});
 	$('#main-nav').localScroll({ duration: 'slow', offset: -94});
 	$('#proyectos-link').localScroll({ duration: 'slow', offset: -94});
 
@@ -41,6 +44,8 @@ $(window).resize( function(){
 
 	$('#page-container').css("margin-top", height + "px");
 	$('#foto-intro').height( height - $('#main-nav').height() - 42 );
+
+	$('#logo-intro').css("margin-top", (height - $('#logo-intro').height())/2 + "px" );
 
 	if( height < 740){
 		$('#container-quienes').css("margin-top", ($('#foto-intro').height()) * .4 + "px")
